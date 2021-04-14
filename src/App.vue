a47ee8802790a307780be68649f2ca6a946b28b7
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-04-14 09:43:44 +0200

    Create two objects in App.vue and import App.vue to main.js

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,20 @@
 const app = {
   data() {
     return {
-      friends: [],
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45 678",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 65 443",
+          email: "julie@localhost.com",
+        },
+      ],
     };
   },
 };
