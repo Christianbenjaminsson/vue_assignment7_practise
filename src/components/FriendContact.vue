3696ae32717c4d5e8a7bfb9dd8f05249b3b288ca
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-04-16 16:24:56 +0200

    Create button and a list

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,11 +1,15 @@
 <template>
   <li>
     <h2>{{ friend.name }}</h2>
+    <button>Show Details</button>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </li>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
